39ce4830c5e93621bb97e419bf85558fa6285162
Steven Armstrong <steven.armstrong@inf.ethz.ch>
2011-06-17 08:17:54 +0200

    fix url to project page
    Signed-off-by: Steven Armstrong <steven.armstrong@inf.ethz.ch>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version=__version__,
     author='Steven Armstrong',
     author_email='steven-%s@armstrong.cc' % name,
-    url='http://github.com/ungleich/%s/' % name,
+    url='http://github.com/asteven/%s/' % name,
     description='Simple PYTHONPATH handling',
     py_modules=[name],
 )
